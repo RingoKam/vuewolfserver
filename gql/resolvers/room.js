37b83ac5dec11b736b14b768c7131333ebfe6765
ringo kam <ringo_kam@hotmail.com>
2018-11-26 01:53:44 -0500

    WIP filter subscription

--- gql/resolvers/room.js
+++ gql/resolvers/room.js
@@ -1,33 +1,54 @@
 const { PubSub } = require("apollo-server");
+const { withFilter } = require("apollo-server");
 const pubsub = new PubSub();
+const _ = require("lodash");
 
 //Data
 const { get, newRoom, joinRoom } = require("../../data/room");
+const player = require("../../data/player");
 
 //Event
 const updateRoom = "UpdateRoom";
 
 module.exports = {
-  Query: {
-    getRoom: (root, { roomId }, context) => {
-      return get(roomId);
-    }
-  },
-  Mutation: {
-    newRoom: (root, { roomName, playerId }, context) => {
-      const room = newRoom(roomName, playerId);
-      pubsub.publish(updateRoom, { updateRoom: room});
-      return room.id;
+    Query: {
+        getRoom: (root, { roomId }, context) => {
+            const r = get(roomId);
+            const p = player.get();
+            return r;
+        }
     },
-    joinRoom: (root, { roomId, playerId }, context) => {
-      const room = joinRoom(roomId, playerId);
-      pubsub.publish(updateRoom, { updateRoom: room});
-      return roomId;
+    Mutation: {
+        newRoom: (root, { roomName, player }, context) => {
+            const room = newRoom(roomName, player);
+            pubsub.publish(updateRoom, { updateRoom: room });
+            return room.id;
+        },
+        joinRoom: (root, { roomId, player }, context) => {
+            const room = joinRoom(roomId, player);
+            pubsub.publish(updateRoom, { updateRoom: room });
+            return roomId;
+        },
+        removePlayer: (root, {roomId, playerId}, context) => {
+            return playerId;
+        },
+        startGame(root, { roomId }, context) {
+            return playerId;
+        }
+
+    },
+    Subscription: {
+        updateRoom: {
+            subscribe: withFilter(
+                () => pubsub.asyncIterator([updateRoom]), 
+                (payload, variable) => {
+                    const payloadRoomId = _.get(payload, "updateRoom.roomId", false);
+                    const isSameRoom = payloadRoomId === variable.roomId;
+                    const provideFilter = !!variable.roomId;
+                    return isSameRoom || !provideFilter;
+                }
+            )
+            // subscribe: () => pubsub.asyncIterator([updateRoom], withFilter)
+        }
     }
-  },
-  Subscription: {
-    updateRoom: {
-      subscribe: () => pubsub.asyncIterator([updateRoom])
-    }
-  }
 };
--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const { typeDefs, resolvers } = require("./gql");
 const { makeExecutableSchema } = require("graphql-tools");
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ schema, subscriptions: {
+  onConnect: (connectionParams, webSocket, context) => {
+    console.log(connectionParams, webSocket, context)
+  },
+  onDisconnect: (websocket, context) => {
+    console.log(websocket, context)
+  }
+}});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
